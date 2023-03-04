753122d8612c2c146fddda7d48f3ad6e88d89e92
Piotr Wisniewski <piowisni@gmail.com>
2023-03-04 08:43:43 +0100

    added first Get Methods

---
+++ InfoShare.Rest.FlightBook.API/Controllers/NumbersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfoShare.Rest.FlightBook.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumbersController : ControllerBase
+    {
+        [HttpGet("random")]
+        public int GetNumber()
+        {
+            return 1;
+        }
+
+        [HttpGet("multiplication/{a}/{b}")]  // Mapowanie URL do parametrów metody
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
